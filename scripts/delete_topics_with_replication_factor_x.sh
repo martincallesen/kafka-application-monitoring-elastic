2cd67c7b64ead422f7730b2ab5b0a68ceb9e3de8
Sion Smith <sion.smith@gmail.com>
2020-08-18 11:51:44 +0100

    updated script to use correct grep! LOL

--- scripts/delete_topics_with_replication_factor_x.sh
+++ scripts/delete_topics_with_replication_factor_x.sh
@@ -8,7 +8,7 @@ ALL_TOPICS=$(kafka-topics --bootstrap-server localhost:9092 --lists)
 # Loop over all topics and check replication factor
 for TOPIC in $ALL_TOPICS
 do
-    RESULT=$(kafka-topics --bootstrap-server localhost:9092 --describe --topic $TOPIC | grep "Replicas: $REPLICATION_FACTOR" | awk '{print $2}')
+    RESULT=$(kafka-topics --bootstrap-server localhost:9092 --describe --topic $TOPIC | grep "ReplicationFactor: $REPLICATION_FACTOR" | awk '{print substr($1,7)}')
     if [[ "${RESULT}" != -z ]]; then
       kafka-topics --bootstrap-server localhost:9092 --delete --topic $TOPIC
     fi
